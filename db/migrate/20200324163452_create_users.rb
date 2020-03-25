77364d312be4da122a2776f01dd04691570236d0
angeloxenakis <angeloxenakis@gmail.com>
2020-03-25 14:21:45 -0500

    added password functionality and made login creation names unique

--- db/migrate/20200324163452_create_users.rb
+++ db/migrate/20200324163452_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |user|
       user.string :name
+      user.string :password
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_163459) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "password"
   end
 
 end
