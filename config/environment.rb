b9f129b472128690e05079c7821116215d2f3342
willfbren <willfbren@gmail.com>
2020-03-26 15:23:51 -0500

    rspec init

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,19 @@ Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
-ActiveRecord::Base.establish_connection(
-    adapter:'sqlite3',
-    database:"db/development.sqlite"
+if ENV['ACTIVE_RECORD_ENV'] == 'test'
+    # Mock Database
+    # Fake Database to use while our tests run
+    ActiveRecord::Base.logger = nil
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: ":memory:"
     )
+else
+    ActiveRecord::Base.establish_connection(
+        adapter: 'sqlite3',
+        database: "db/development.sqlite"
+    )
+end
 
 require_all 'app'
